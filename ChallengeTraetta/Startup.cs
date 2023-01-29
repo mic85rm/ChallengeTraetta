51933db76b745d7f5c2c95553ddf2371ceb76962
mtraetta <mictrt85@gmail.com>
2023-01-29 18:52:42 +0100

    secondo rilascio

--- ChallengeTraetta/Startup.cs
+++ ChallengeTraetta/Startup.cs
@@ -12,8 +12,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,8 +54,8 @@ namespace ChallengeTraetta
       ValidateIssuerSigningKey = true,
       ValidIssuer = Configuration["Jwt:Issuer"],
       ValidAudience = Configuration["Jwt:Audience"],
-      
-    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"])),
+
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"])),
       ClockSkew = TimeSpan.Zero
     };
   });
@@ -61,6 +65,41 @@ namespace ChallengeTraetta
         config.AddPolicy(Policies.Admin, Policies.AdminPolicy());
         config.AddPolicy(Policies.User, Policies.UserPolicy());
       });
+
+
+      services.AddSwaggerGen(options =>
+      {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+          Name = "Authorization",
+          Type = SecuritySchemeType.ApiKey,
+          Scheme = "Bearer",
+          BearerFormat = "JWT",
+          In = ParameterLocation.Header,
+          Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+ {
+     {
+           new OpenApiSecurityScheme
+             {
+                 Reference = new OpenApiReference
+                 {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "Bearer"
+                 }
+             },
+             new string[] {}
+     }
+ });
+        options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+          Title = "Place Info Service API",
+          Version = "v2",
+          Description = "Sample service for Learner",
+        });
+      });
+
 
     }
 
@@ -74,13 +113,15 @@ namespace ChallengeTraetta
 
       context.Seed();
 
-     
+
+
+      app.UseSwagger();
+      app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v2/swagger.json", "PlaceInfo Services"));
+    
 
 
 
-
-
-      app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
       app.UseRouting();
       app.UseAuthentication();
